3685b3e77927f75abe4dbb350e8ff44aea54c5dc
Kiryl Pryhozhy <kirill19962003@gmail.com>
2023-04-09 19:36:23 +0300

    feat: implement controllers

--- src/main/java/com/online/store/controller/ProductController.java
+++ src/main/java/com/online/store/controller/ProductController.java
@@ -6,7 +6,11 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,11 +23,33 @@ public class ProductController {
     private final Logger logger = LoggerFactory.getLogger(ProductController.class);
     private final ProductService productService;
 
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable String id) {
+        Product content = productService.findById(Integer.parseInt(id))
+                .orElse(null);
+        return ResponseEntity.ok(content);
+    }
+
     @PostMapping
     public ResponseEntity<Product> createProduct(@RequestBody Product product) {
-        logger.info("POST request body: " + product);
-        Product product1 = productService.create(product);
+        Product content = productService.create(product);
         return ResponseEntity.ok()
-                .body(product1);
+                .body(content);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable String id) {
+        productService.deleteById(Integer.parseInt(id));
+        return ResponseEntity.ok("");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable String id,
+                                                 @RequestBody Product product) {
+        Product updatedProduct = productService.update(Integer.parseInt(id), product);
+        return ResponseEntity.ok(updatedProduct);
+    }
+
+
 }
--- src/main/java/com/online/store/controller/UserController.java
+++ src/main/java/com/online/store/controller/UserController.java
@@ -1,5 +1,53 @@
 package com.online.store.controller;
 
+import com.online.store.model.User;
+import com.online.store.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/users", produces = "application/json")
+@RequiredArgsConstructor
 public class UserController {
 
+    private final Logger logger = LoggerFactory.getLogger(ProductController.class);
+    private final UserService userService;
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable String id) {
+        User content = userService.findById(Integer.parseInt(id))
+                .orElse(null);
+        return ResponseEntity.ok(content);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User content = userService.create(user);
+        return ResponseEntity.ok()
+                .body(content);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable String id) {
+        userService.deleteById(Integer.parseInt(id));
+        return ResponseEntity.ok("Entity was deleted");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable String id,
+                                           @RequestBody User user) {
+        User updatedUser = userService.update(Integer.parseInt(id), user);
+        return ResponseEntity.ok(updatedUser);
+    }
 }
