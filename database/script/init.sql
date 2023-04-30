95d1de3552693164bfe6b309bf284b664190af1c
Kiryl Pryhozhy <kirill19962003@gmail.com>
2023-05-01 01:33:54 +0300

    fix: make db initialization work

--- database/script/init.sql
+++ database/script/init.sql
@@ -1,27 +1,33 @@
 -- CREATE DATABASE store;
 --
 -- \connect store
-
 CREATE SCHEMA IF NOT EXISTS online_store;
 
-CREATE TABLE IF NOT EXISTS online_store.products
-(
-    id    SERIAL,
-    name  varchar(100)     not null,
+CREATE TABLE IF NOT EXISTS online_store.products (
+    id SERIAL,
+    name varchar(100) not null,
     price double precision not null,
-    type  varchar(20),
+    type varchar(20),
     brand varchar(50)
 );
 
-CREATE TABLE IF NOT EXISTS online_store.users
-(
-    id       serial,
+CREATE TABLE IF NOT EXISTS online_store.users (
+    id serial,
     username varchar(50) not null,
-    email    varchar(50) not null
+    email varchar(50) not null
 );
 
-insert into online_store.users(username, email) values ('111', 'email111');
-insert into online_store.users(username, email) values ('222', 'email222');
-insert into online_store.users(username, email) values ('333', 'email333');
-insert into online_store.users(username, email) values ('444', 'email444');
-insert into online_store.users(username, email) values ('555', 'email555');
+INSERT INTO
+    online_store.users(username, email)
+VALUES
+    ('handsome', 'handsome@gmail.com');
+
+INSERT INTO
+    online_store.users(username, email)
+VALUES
+    ('kaprizner', 'kaprizner@gmail.com');
+
+INSERT INTO
+    online_store.users(username, email)
+VALUES
+    ('savonchiks', 'egorrr@gmail.com');
